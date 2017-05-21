768a865a503ffe68670e38b68cf2276d1ac4b557
Gilberto Vargas <tachoguitar@gmail.com>
2017-05-20 19:28:36 -0500

    Add a plan for migrations

--- src/MiniSequel/Migration.hs
+++ src/MiniSequel/Migration.hs
@@ -1,3 +1,24 @@
 module MiniSequel.Migration
 where
+  import MiniSequel
+  import MiniSequel.Model
+  import MiniSequel.Expression
 
+  data SequelMigration a =
+    CreateTable (Model a) |
+    AlterTable [SequelTableModifier] |
+    CreateView SequelExpression SequelQuery
+
+  data SequelTableModifier =
+    AddColumn |
+    AddIndex  |
+    AlterColumn |
+    RenameTable  |
+    AddConstraint |
+    AddKey |
+    DropColumn
+
+  data SequelSchemaVersion = SequelSchemaVersion {
+    version :: Int,
+    updateDate :: String
+  }
