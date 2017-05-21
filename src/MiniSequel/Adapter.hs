ee0f5d63085a1436e6d624b8e14dd74f21a1a174
Gilberto Vargas <tachoguitar@gmail.com>
2017-05-20 19:25:23 -0500

    Create MySQL adapter

--- src/MiniSequel/Adapter.hs
+++ src/MiniSequel/Adapter.hs
@@ -4,13 +4,29 @@ where
   import MiniSequel
   import MiniSequel.Model
   import MiniSequel.Expression
+  import MiniSequel.Mapper
 
-  data SequelAdapterDriver = MySQLDriver | PostgreSQLDriver
+  import qualified Database.HDBC as DB
+  import qualified Database.HDBC.MySQL as MSQL
+  import qualified Database.HDBC.PostgreSQL as PSQL
+
+  data SequelAdapterDriver = MySQLDriver | PostgreSQLDriver deriving (Show)
+
   data SequelAdapterConfig = SequelAdapterConfig {
     driver :: SequelAdapterDriver,
     host :: String,
-    port :: String,
+    port :: Int,
     user :: String,
     password :: String,
-    database :: String
+    database :: String,
+    maxConnections :: Int
   }
+
+  mySQLConnector conf =
+    MSQL.connectMySQL MSQL.defaultMySQLConnectInfo {
+      MSQL.mysqlHost       = host conf,
+      MSQL.mysqlUser       = user conf,
+      MSQL.mysqlPassword   = password conf,
+      MSQL.mysqlPort       = port conf,
+      MSQL.mysqlDatabase   = database conf
+    }
